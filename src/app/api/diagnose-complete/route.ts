66b2bc032cb998a0c469917db634e20e3b7297e5
Maryam Hamdy <hamdymaryam874@gmail.com>
2026-07-15 18:36:33 +0300

    Update project

--- src/app/api/diagnose-complete/route.ts
+++ src/app/api/diagnose-complete/route.ts
@@ -1,15 +1,13 @@
 // 
-
-
+import { NextRequest } from "next/server";
 import { cookies } from "next/headers";
 
 const API_BASE = "https://dermamind-api-production-a383.up.railway.app";
 
-export async function POST(req) {
+export async function POST(req: NextRequest) {
   const body = await req.json();
   const token = (await cookies()).get("token")?.value;
-
-  console.log("🟩 [diagnose-complete] TOKEN PRESENT? =>", Boolean(token));
+   console.log("🟩 [diagnose-complete] TOKEN PRESENT? =>", Boolean(token));
 
   try {
     const response = await fetch(`${API_BASE}/api/DermaScan/diagnose/complete`, {
--- src/app/api/diagnose-start/route.ts
+++ src/app/api/diagnose-start/route.ts
@@ -1,38 +1,13 @@
-// import { cookies } from "next/headers";
 
-// export async function POST(req) {
-//   const formData = await req.formData();
-//   const token = (await cookies()).get("token")?.value;
-
-//   try {
-//     const response = await fetch(
-//       "https://dermamind-api-production-a383.up.railway.app/api/DermaScan/diagnose/start",
-//       {
-//         method: "POST",
-//         headers: {
-//           ...(token ? { Authorization: `Bearer ${token}` } : {}),
-//         },
-//         body: formData,
-//       }
-//     );
-
-//     const data = await response.json();
-
-//     return Response.json(data, { status: response.status });
-//   } catch (error) {
-//     return Response.json({ error: "Failed to fetch" }, { status: 500 });
-//   }
-// }
-
-
+import { NextRequest } from "next/server";
 import { cookies } from "next/headers";
 
 const API_BASE = "https://dermamind-api-production-a383.up.railway.app";
 
-export async function POST(req) {
+export async function POST(req: NextRequest) {
   const formData = await req.formData();
   const token = (await cookies()).get("token")?.value;
-
+ 
   console.log("🟦 [diagnose-start] TOKEN PRESENT? =>", Boolean(token));
 
   try {
